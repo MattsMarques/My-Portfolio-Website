73e5a8beb2334a7c32b943c5ee5bf4e35dd5ce3e
MattsMarques <158483745+MattsMarques@users.noreply.github.com>
2026-02-27 01:01:23 -0300

    Layout Mobile

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const navmenu = document.getElementById("nav-menu")
+
+function openNavbar(){
+    navmenu.classList.add('show')
+}
+
+function closeNavbar(){
+    navmenu.classList.remove('show')
+}
